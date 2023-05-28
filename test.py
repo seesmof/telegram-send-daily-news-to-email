841f4826be0a2739ed013cecdb2b0dce97d888bf
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-28 13:25:06 +0000

    🌿: Sunday, May 28, 2023 at 4:25:06 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,22 +1,30 @@
-import smtplib
+import telethon
+TOKEN = "6055667715:AAGlgPKBJftI_Cks1m8n9n_rSpqhN5bXrzg"
+CHANNEL_ID = "@mindua"
+APP_ID = "29709258"
+APP_HASH = "545b92e022b3885152a76625cd74c9d5"
+TO_LOOK_FOR = "#фотодня"
 
-sender = 'seesmwork@gmail.com'
-receiver = 'seesmwork@gmail.com'
-password = 'p0zBE5e146898992'
-subject = "Python email test"
-body = "I wrote an email! :D"
 
-# header
-message = f"""From: {sender}
-To: {receiver}
-Subject: {subject}\n
-{body}
-"""
+# Create a Telegram client object
+client = telethon.TelegramClient(
+    'my_bot', api_id=APP_ID, api_hash=APP_HASH)
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
+# Connect to the Telegram servers
+client.connect()
 
-server.login(sender, password)
-print("Login successful")
-server.sendmail(sender, receiver, message)
-print("Email sent successfully")
+# Get the channel ID
+channel_id = CHANNEL_ID
+
+# Start monitoring the channel for messages with the specific string
+while True:
+    # Get the latest messages from the channel
+    messages = client.get_messages(channel_id)
+
+    # For each message, check if it contains the specific string
+    for message in messages:
+        if message.text == TO_LOOK_FOR:
+            # If the message contains the specific string, forward it to your account
+            client.forward_messages(message.chat_id, message.id)
+
+# Keep monitoring the channel for messages
