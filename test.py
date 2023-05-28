cdf83351a3c4901eb6b6f2d85fd1ed2b4896986e
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-28 13:02:40 +0000

    🌿: Sunday, May 28, 2023 at 4:02:40 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-import yagmail
+import smtplib
 
-from_email = 'seesvalorant@gmail.com'
-to_email = 'seesmwork@gmail.com'
-password = ',X)kC9-)}svS"n_d'
+sender = 'seesmwork@gmail.com'
+receiver = 'seesmwork@gmail.com'
+password = 'p0zBE5e146898992'
+subject = "Python email test"
+body = "I wrote an email! :D"
 
-yag = yagmail.SMTP(from_email, password)
-yag.send(to=to_email, subject="Hi",
-         contents="Hello, this is a test email from Python!")
+# header
+message = f"""From: {sender}
+To: {receiver}
+Subject: {subject}\n
+{body}
+"""
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+
+server.login(sender, password)
+print("Login successful")
+server.sendmail(sender, receiver, message)
+print("Email sent successfully")
