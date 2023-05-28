01118e471999904a2a6185d669cd583b77b53854
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-28 16:36:40 +0300

    modified main.py and session_name.session in .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, events
+from telethon.tl.types import InputPeerUser
 import asyncio
 
 TOKEN = "6055667715:AAGlgPKBJftI_Cks1m8n9n_rSpqhN5bXrzg"
@@ -12,19 +13,10 @@ async def main():
     await client.start()
     channel = await client.get_entity(CHANNEL_ID)
     resulting_news = ''
-    async for message in client.iter_messages(channel, search='#фотодня', limit=1):
+    async for message in client.iter_messages(channel, search='#фотодня', limit=4):
         resulting_news += message.text
-    resulting_news = resulting_news.replace('*', '')
-    resulting_news = resulting_news.replace(' — у світлинах', '')
-    resulting_news = resulting_news.replace(
-        '[📱 Підписатись на Майнд пояснює](https://t.me/mindua)', '')
-    resulting_news = resulting_news.replace(
-        '#фотодня #дайджест #важливе', '')
-    resulting_news = resulting_news.replace(
-        'Повна інформаційна картина дня — на каналі [Mind tweet](https://t.me/mindUAtweet)', '')
-    resulting_news = resulting_news.replace(
-        '🛑', '- ')
     print(resulting_news)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
